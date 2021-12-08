4036b9da5b7d2da5d6b9cec9ce388d39afcf8cb5
saman-zdf <samanfyz68@gmail.com>
2021-12-08 12:11:20 +0800

    configured database, adding connect.js and deleted extra controller file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const connectDB = require('./src/database/connect');
 
 const PORT = process.env.PORT || 5000;
 
@@ -11,6 +13,16 @@ const issuesRouter = require('./src/routes/issuesRoutes');
 
 app.use('/issues', issuesRouter);
 
-app.listen(PORT, () => {
-  console.log(`server is running on port ${PORT}`);
-});
+const start = () => {
+  try {
+    connectDB(process.env.DATABASE_URI);
+    console.log('database is connected');
+    app.listen(PORT, () => {
+      console.log(`server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+start();
