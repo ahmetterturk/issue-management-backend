8c2d461b0261cacc4ae9b7fcb7fbf0a9ff5c0bb3
saman-zdf <samanfyz68@gmail.com>
2021-12-08 11:39:24 +0800

    added nodemon to package json

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-// middleware 
-app.use(express.json())
+// middleware
+app.use(express.json());
 
-
-// issue router 
+// issue router
 const issuesRouter = require('./src/routes/issuesRoutes');
 
-app.use('/issues', issuesRouter)
-
+app.use('/issues', issuesRouter);
 
 app.listen(PORT, () => {
-    console.log(`server is running on port ${PORT}`)
-})
+  console.log(`server is running on port ${PORT}`);
+});
