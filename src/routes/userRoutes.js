eb88f08fae6014eb5a0a05de342e807ba287438a
saman-zdf <samanfyz68@gmail.com>
2021-12-27 14:44:42 +0800

    added updateUser in user controller, used mongoose middleware to create jwt and hash password before create user

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const { signIn, signUp } = require('../controllers/usersController');
+const {
+  signIn,
+  signUp,
+  updateUser,
+} = require('../controllers/usersController');
 
 router.post('/signin', signIn);
 router.post('/signup', signUp);
+router.patch('/:id', updateUser);
 
 module.exports = router;
